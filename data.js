a1964eac68ff37f9ac73b53bf1708687cb89a426
satriayudhia <satria.yudhiap@gmail.com>
2023-09-13 12:31:02 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,351 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-lobby",
+      "name": "1. LOBBY",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.71979404913699,
+        "pitch": 0.169984031411488,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21868539417426724,
+          "pitch": 0.05189333520688777,
+          "rotation": 10.995574287564278,
+          "target": "2-5-run-way"
+        },
+        {
+          "yaw": 1.1516377381497236,
+          "pitch": 0.023013526411567398,
+          "rotation": 14.137166941154074,
+          "target": "3-7-parking-area"
+        },
+        {
+          "yaw": -2.284673648270706,
+          "pitch": 0.053893478309934295,
+          "rotation": 0,
+          "target": "1-2a-unit---living"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2a-unit---living",
+      "name": "2A. UNIT - LIVING",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.8809912147819361,
+        "pitch": 0.36415479908682613,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3798875653272384,
+          "pitch": 0.02694845459807027,
+          "rotation": 0,
+          "target": "0-1-lobby"
+        },
+        {
+          "yaw": 2.453282490717158,
+          "pitch": 0.23481952314088161,
+          "rotation": 1.5707963267948966,
+          "target": "7-2e-unit---kamar-mandi"
+        },
+        {
+          "yaw": -3.102503780485801,
+          "pitch": 0.22159844084539415,
+          "rotation": 0,
+          "target": "4-2b-unit---kamar-1"
+        },
+        {
+          "yaw": -2.6402598358360088,
+          "pitch": 0.28594739823724247,
+          "rotation": 0,
+          "target": "5-2c-unit---kamar-2"
+        },
+        {
+          "yaw": -1.268939867401162,
+          "pitch": 0.23063651859062162,
+          "rotation": 0,
+          "target": "6-2d-unit---kamar-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-5-run-way",
+      "name": "5. RUN WAY",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.7222633264618992,
+        "pitch": -0.0048116588536153415,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4628860505538794,
+          "pitch": 0.025241971806451602,
+          "rotation": 0,
+          "target": "0-1-lobby"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-7-parking-area",
+      "name": "7. PARKING AREA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.87751820408859,
+        "pitch": 0.17783421513878395,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.23471752065647777,
+          "pitch": 0.07670922226690635,
+          "rotation": 0,
+          "target": "0-1-lobby"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-2b-unit---kamar-1",
+      "name": "2B. UNIT - KAMAR 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.4862744633996403,
+        "pitch": 0.15157733368594606,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8772413346786472,
+          "pitch": 0.19850814121707394,
+          "rotation": 0,
+          "target": "1-2a-unit---living"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-2c-unit---kamar-2",
+      "name": "2C. UNIT - KAMAR 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.0268642534480676,
+        "pitch": 0.2419987699442352,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.408189012704522,
+          "pitch": 0.24440505187961392,
+          "rotation": 0,
+          "target": "1-2a-unit---living"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-2d-unit---kamar-3",
+      "name": "2D. UNIT - KAMAR 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.7563136379634638,
+        "pitch": 0.1857209583309931,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9812362632263323,
+          "pitch": 0.1864217968265649,
+          "rotation": 0,
+          "target": "1-2a-unit---living"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-2e-unit---kamar-mandi",
+      "name": "2E. UNIT - KAMAR MANDI",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.925152831952744,
+        "pitch": 0.1015206723313149,
+        "fov": 1.3672870777451915
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.25738837873566567,
+          "pitch": 0.46551124909831465,
+          "rotation": 0,
+          "target": "1-2a-unit---living"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
